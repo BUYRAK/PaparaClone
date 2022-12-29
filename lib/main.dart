3a6c257996533ae23ffb7f30fbfbea8ae02b15a3
buyrak <siyahmerkez@gmail.com>
2022-12-29 05:26:04 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:papara_clone/splash_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      // ignore: prefer_const_constructors
+      home: Scaffold(
+        // ignore: prefer_const_constructors
+        body: SplashScreen()
+      ),
+    );
+  }
+}
---
+++ lib/splash_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Image.asset('assets/img/papara_logo.png', height: 250, width: 250,),
+      ),
+    );
+  }
+}
